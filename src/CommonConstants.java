ee5cca2d0a920b592ea9561ea252e389562f070c
JacobWesthoff04 <mzufnz@gmail.com>
2025-04-17 01:10:21 -0500

    Made GUI

---
+++ src/CommonConstants.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+
+public class CommonConstants {
+    //Color config
+    public static final Color BACKGROUND_COLOR = Color.decode("#011627");
+    public static final Color X_COLOR = Color.decode("#E71D36");
+    public static final Color O_COLOR = Color.decode("#2EC4B6");
+    public static final Color BAR_COLOR = Color.decode("#FF9F1C");
+    public static final Color BOARD_COLOR = Color.decode("#FDFFFC");
+
+    //Size config
+    public static final Dimension FRAME_SIZE = new Dimension(540, 760);
+
+    //Value config
+    public static final String X_LABEL = "X";
+    public static final String O_LABEL = "O";
+}
